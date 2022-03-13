e53df27b0346f7d1640bf5a04b9f21ab16608468
barbadeadesh <barbadeadesh2020@email.com>
2022-03-13 19:51:25 +0530

    Check the number is prime or not prime

---
+++ Uc3.java
@@ -0,0 +1,26 @@
+ 
+import java.util.Scanner;
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the number:");
+      int no=sc.nextInt();
+      int temp=0;
+      for(int i=2; i<=no-1; i++) {
+    	  
+    	  if(no%i==0) {
+    		  temp = temp+1;
+    	  }
+      }
+      if(temp==0) {
+    	  System.out.println(no+" is prime Number");
+      }
+      else {
+    	  System.out.println(no+" is not prime Number");
+      }
+      
+	}
+
+}
